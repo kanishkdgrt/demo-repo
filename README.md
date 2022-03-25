f190c735b05830c104345b56799b6a5fb614cc1f
kanishkdgrt <jabsdbaa@gmail.com>
2022-03-25 22:40:31 +0530

    new python file
    python everywhere

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # This a learning project for git and github
 
 Hello  !
+hi guys how are you
+learning git and github
+##subheader
+ going good still now
---
+++ just.py
@@ -0,0 +1,6 @@
+import os
+print("Welcome to my github repository")
+name=input('Enter your name sir !:')
+length=name.len()
+print('your name has letter',length)
+
